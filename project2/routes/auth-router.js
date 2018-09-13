d44bfb18d18821d403890778669f00af6705b41d
Julie Ménard <menard_julie@ymail.com>
2018-09-13 11:49:34 +0200

    css corrigé

--- project2/routes/auth-router.js
+++ project2/routes/auth-router.js
@@ -7,22 +7,21 @@ const passport = require("passport");
 
 ///////////ROUTE SIGN UP/////////////////////////////////////////////////////////////////////
 router.get("/signup", (req,res,next) => {
-  res.render("./auth-views/signup-form.hbs");
+ res.render("./auth-views/signup-form.hbs");
 });
 
 router.post("/process-signup", (req,res,next) => {
-  const {fullName, email, originalPassword, zodiac, location} = req.body;
-  let possession = [];
-  //Encrypt the submitted password
-  const encryptedPassword= bcrypt.hashSync(originalPassword, 10);
+ const {fullName, email, originalPassword, zodiac, location} = req.body;
+ let possession = [];
+ //Encrypt the submitted password
+ const encryptedPassword= bcrypt.hashSync(originalPassword, 10);
 
-  User.create ({fullName, email, encryptedPassword, possession, zodiac, location})
-  .then(userDoc => {
-    req.flash("success", "Sign up success !");
-    //req.flash("success", "Sign up success! 🖖🏾");
-    res.redirect("/");
-    })
-  .catch(err => next(err));
+ User.create ({fullName, email, encryptedPassword, possession, zodiac, location})
+ .then(userDoc => {
+   req.flash("success", "Sign up success! :tada::tada::tada:");
+   res.redirect("/");
+   })
+ .catch(err => next(err));
 });
 
 
@@ -30,58 +29,58 @@ router.post("/process-signup", (req,res,next) => {
 
 ///////////ROUTE LOG IN/////////////////////////////////////////////////////////////////////
 router.get("/login", (req,res,next)=> {
-  res.render("./auth-views/login-form")
+ res.render("./auth-views/login-form")
 });
 
 router.post("/process-login", (req, res, next) => {
-  const {email, originalPassword} = req.body;
-  User.findOne({email : {$eq:email}})
-  .then(userDoc => {
-    if (!userDoc){
-      req.flash("error", "Incorrect email");
-      res.redirect("./auth-views/login-form.hbs");
-      return;
-      res.redirect("./login-form");
-      return; // use "return instead of a big else {}"
-    }
-      const {encryptedPassword} = userDoc;
-      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
-        req.flash("error", "Incorrect password");
-        res.redirect("./auth-views/login-form.hbs");
-        return;
-      }
-      req.logIn(userDoc, () => {
-      req.flash("success", "Log in success!");
-      res.redirect("/");
-      })
-           
-    })
+ const {email, originalPassword} = req.body;
+ User.findOne({email : {$eq:email}})
+ .then(userDoc => {
+   if (!userDoc){
+     req.flash("error", "Incorrect email :cœur_brisé::cœur_brisé::cœur_brisé:");
+     res.redirect("./auth-views/login-form.hbs");
+     return;
+     res.redirect("./login-form");
+     return; // use "return instead of a big else {}"
+   }
+     const {encryptedPassword} = userDoc;
+     if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+       req.flash("error", "Incorrect password :cœur_brisé::cœur_brisé::cœur_brisé:");
+       res.redirect("./auth-views/login-form.hbs");
+       return;
+     }
+     req.logIn(userDoc, () => {
+     req.flash("success", "Congrats you're logged in! :champagne::champagne::champagne:");
+     res.redirect("/");
+     })
 
-  .catch (err => next(err));
-    });
+   })
+
+ .catch (err => next(err));
+   });
 
 ///////////ROUTE LOG OUT/////////////////////////////////////////////////////////////////////
-    router.get("/logout", (req,res,next) => {
-      req.logOut();
-      req.flash("success", "Logged out succesfully");
-      res.redirect("/");
-    });
+   router.get("/logout", (req,res,next) => {
+     req.logOut();
+     req.flash("success", "See you later! :salut_main::salut_main::salut_main:");
+     res.redirect("/");
+   });
 
 ///////////ROUTE GOOGLE LOG IN///////////////////////////////////////////////////////////////
-    router.get("/google/login", passport.authenticate("google", {
-      scope: [
-        "https://www.googleapis.com/auth/plus.login",
-        "https://www.googleapis.com/auth/plus.profile.emails.read",
-        ]
-    }));
+   router.get("/google/login", passport.authenticate("google", {
+     scope: [
+       "https://www.googleapis.com/auth/plus.login",
+       "https://www.googleapis.com/auth/plus.profile.emails.read",
+       ]
+   }));
 
-    router.get("/google/user-info",
-    passport.authenticate("google", {
-    successRedirect : "/",
-    successFlash:"Google log in successful ! ",
-    failureRedirect: "/login",
-    failureFlash: "Google log in failed !",
-    }));
+   router.get("/google/user-info",
+   passport.authenticate("google", {
+   successRedirect : "/",
+   successFlash:" Congrats you're logged in with Google :champagne::champagne::champagne: ",
+   failureRedirect: "/login",
+   failureFlash: "Google log in failed! :sanglot::sanglot::sanglot:",
+   }));
 
 
 module.exports = router;
